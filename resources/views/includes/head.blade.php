0482fb89f9fec8d8bbbba385d21198c7b78c51b6
Blazej Kedzierski <blazej.kedzierski@op.pl>
2021-01-25 15:59:33 +0100

    Basic styles and templates - progress

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" type="text/css"> 
+    <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ URL::asset('libs/slick/slick.css') }}" type="text/css"> 
+    <link rel="stylesheet" href="{{ URL::asset('libs/slick/slick-theme.css') }}" type="text/css"> 
+    <script type="text/javascript" src="{{ URL::asset('libs/slick/slick.min.js') }}"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="content-container">
+        <div class="menu">
+            <div class="logo">
+                <img src="{{ URL::asset('images/logo/logo.png') }}" />
+            </div>
+            <div class="menu-nav-box">
+                <div class="top-nav">
+                    <div class="test">
+                    TELEFON, FACEBOK, INNE
+                    </div>
+                </div>
+                <div class="bottom-nav">
+                    <ul class="menu__list navbar">
+                        <li class="menu__item"><a href="#">STRONA GŁÓWNA</a></li>
+                        <li class="menu__item"><a href="#">O NAS</a></li>
+                        <li class="menu__item"><a href="#">OFERTA</a></li>
+                        <li class="menu__item"><a href="#">CENNIK</a></li>
+                        <li class="menu__item"><a href="#">GALERIA</a></li>
+                        <li class="menu__item"><a href="#">KONTAKT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
